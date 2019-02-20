e2469a906f24bb5ca382a9c27cff1f1e75f55f23
Andrew Reid East <andrew@andreweast.net>
2019-02-20 23:32:31 +0000

    Return JSON from API

--- src/main/java/net/andreweast/api/ScheduleEndpoint.java
+++ src/main/java/net/andreweast/api/ScheduleEndpoint.java
@@ -1,15 +1,23 @@
 package net.andreweast.api;
 
+import net.andreweast.Schedule;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/schedule")
 public class ScheduleEndpoint {
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getSchedule() {
-        return "I am a banana! (AKA schedule)";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> getSchedule() {
+        List<String> data = new ArrayList<>();
+        data.add("Hello");
+        data.add("World");
+        data.add("schedule API");
+        return data;
     }
 }
