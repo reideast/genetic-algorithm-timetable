8c5c9e8b2b928cbfba628a10086a1298fce237d9
Andrew Reid East <andrew@andreweast.net>
2019-02-20 23:57:02 +0000

    Return the scheduler's output from API

--- src/main/java/net/andreweast/api/ScheduleEndpoint.java
+++ src/main/java/net/andreweast/api/ScheduleEndpoint.java
@@ -6,18 +6,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
 
 @Path("/schedule")
 public class ScheduleEndpoint {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<String> getSchedule() {
-        List<String> data = new ArrayList<>();
-        data.add("Hello");
-        data.add("World");
-        data.add("schedule API");
-        return data;
+    public String getSchedule() {
+        Schedule scheduler = new Schedule();
+        return scheduler.schedule();
+        // TODO: can return a POJO, and all getField() methods will expand into the JSON
     }
 }
