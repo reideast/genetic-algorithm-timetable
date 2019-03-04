ac5445885d71bd0e860be6d348ec23127bbae3ab
Andrew Reid East <andrew@andreweast.net>
2019-03-04 21:06:11 +0000

    Switch to a different JDBC pool provider to get around an AWS Elastic Beanstalk defect with tomcat-dbcp

--- src/main/java/net/andreweast/api/CourseEndpoint.java
+++ src/main/java/net/andreweast/api/CourseEndpoint.java
@@ -25,39 +25,39 @@ import java.sql.Statement;
 @Path("/course")
 public class CourseEndpoint {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String testWrite() {
-        CoursesEntity course = new CoursesEntity();
-        course.setName("1BA");
-        course.setDepartmentId(1);
-        System.out.println("Made a course object: " + course);
-
-        EntityManager em = LocalEntityManagerFactory.createEntityManager();
-        System.out.println("Got an EM: " + em);
-
-        try {
-//            course.setCourseId(2);
-            em.persist(course);
-            // DEBUG: I do not believe this is creating the DB entry
-            System.out.println("Persist has completed");
-
-//            System.out.println("Transaction? " + em.getTransaction());
-//            System.out.println("Is joined to transaction? " + em.isJoinedToTransaction());
-//            em.joinTransaction();
-//            System.out.println("Transaction? " + em.getTransaction());
-//            System.out.println("Is joined to transaction? " + em.isJoinedToTransaction());
+//    @GET
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String testWrite() {
+//        CoursesEntity course = new CoursesEntity();
+//        course.setName("1BA");
+//        course.setDepartmentId(1);
+//        System.out.println("Made a course object: " + course);
 //
+//        EntityManager em = LocalEntityManagerFactory.createEntityManager();
+//        System.out.println("Got an EM: " + em);
 //
-//            System.out.println("Flush mode type: " + em.getFlushMode());
-//            em.flush(); // DEBUG
-//            System.out.println("Flush has completed");
-        } finally {
-            em.close();
-        }
-
-        return "Created";
-    }
+//        try {
+////            course.setCourseId(2);
+//            em.persist(course);
+//            // DEBUG: I do not believe this is creating the DB entry
+//            System.out.println("Persist has completed");
+//
+////            System.out.println("Transaction? " + em.getTransaction());
+////            System.out.println("Is joined to transaction? " + em.isJoinedToTransaction());
+////            em.joinTransaction();
+////            System.out.println("Transaction? " + em.getTransaction());
+////            System.out.println("Is joined to transaction? " + em.isJoinedToTransaction());
+////
+////
+////            System.out.println("Flush mode type: " + em.getFlushMode());
+////            em.flush(); // DEBUG
+////            System.out.println("Flush has completed");
+//        } finally {
+//            em.close();
+//        }
+//
+//        return "Created";
+//    }
 
 //    @GET
 //    @Produces(MediaType.TEXT_PLAIN)
@@ -83,47 +83,47 @@ public class CourseEndpoint {
 
 //    @Resource(name = "java:comp/env/jdbc/postgres", type = DataSource.class)
 //    private DataSource ds;
-//
-//    @GET
-//    @Produces(MediaType.TEXT_PLAIN)
-//    @Path("{course_id}")
-//    public String getVenue(@PathParam("course_id") int id) {
-//
-////        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
-////        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.catalina.util.naming");
-////        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
-//
-//        System.out.println("Attempting to access a course");
-//        System.out.println("ID=" + id);
-//        try
-//        {
-//            String rtn;
-//
-//            Context ctx = new InitialContext();
-////            Context env = (Context) ctx.lookup("java:comp/env");
-//
-////            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/postgres");
-//////            DataSource ds = (DataSource) ctx.lookup("jdbc/postgres");
-//
-//            Connection conn = ds.getConnection();
-//
-//            rtn = "Got Connection " + conn.toString() + "<br>";
-//            Statement stmt = conn.createStatement();
-//            ResultSet rst = stmt.executeQuery("select name from courses where course_id = " + id);
-//
-//            if (rst.next()) {
-//                rtn += "Found!: " + rst.getString(1);
-//            }
-//
-//            conn.close();
-//
-//            return rtn;
-//        } catch (SQLException  e) {
-//            throw new RuntimeException(e);
-//        } catch (NamingException  e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("{course_id}")
+    public String getVenue(@PathParam("course_id") int id) {
+
+//        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+//        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.catalina.util.naming");
+//        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+
+        System.out.println("Attempting to access a course");
+        System.out.println("ID=" + id);
+        try
+        {
+            String rtn;
+
+            Context ctx = new InitialContext();
+//            Context env = (Context) ctx.lookup("java:comp/env");
+
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/postgres");
+//            DataSource ds = (DataSource) ctx.lookup("jdbc/postgres");
+
+            Connection conn = ds.getConnection();
+
+            rtn = "Got Connection " + conn.toString() + "<br>";
+            Statement stmt = conn.createStatement();
+            ResultSet rst = stmt.executeQuery("select name from courses where course_id = " + id);
+
+            if (rst.next()) {
+                rtn += "Found!: " + rst.getString(1);
+            }
+
+            conn.close();
+
+            return rtn;
+        } catch (SQLException  e) {
+            throw new RuntimeException(e);
+        } catch (NamingException  e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /*
 
