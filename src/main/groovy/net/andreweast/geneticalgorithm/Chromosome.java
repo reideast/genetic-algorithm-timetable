0343c35900a22ec32b04d18a68271038fccc1d3b
Andrew Reid East <andrew@andreweast.net>
2019-03-12 18:27:31 +0000

    Convert Chromosome to Java

---
+++ src/main/groovy/net/andreweast/geneticalgorithm/Chromosome.java
@@ -0,0 +1,114 @@
+package net.andreweast.geneticalgorithm;
+
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+
+import java.util.Random;
+
+public class Chromosome implements Comparable<Chromosome> {
+    private static Random random = new Random();
+
+    private ScheduledCourse[] courses;
+    private int storedFitness;
+    private boolean isValidSolution;
+
+    public Chromosome() {
+        courses = new ScheduledCourse[Course.getAllCourses().length];
+        for (int i = 0; i < Course.getAllCourses().length; ++i) {
+            courses[i] = new ScheduledCourse(Course.getAllCourses()[i]);
+        }
+
+        storedFitness = calculateFitness(); // Also sets isValidSolution
+    }
+
+    public Chromosome(Chromosome toClone) {
+        courses = new ScheduledCourse[Course.getAllCourses().length];
+        for (int i = 0; i < Course.getAllCourses().length; ++i) {
+            courses[i] = new ScheduledCourse(
+                    toClone.courses[i].getCourse(),
+                    toClone.courses[i].getVenue(),
+                    toClone.courses[i].getTimeSlot()
+            );
+        }
+
+        storedFitness = toClone.getStoredFitness();
+        isValidSolution = toClone.isValidSolution();
+    }
+
+    public void crossover(Chromosome toCrossWith) {
+        final int crossoverPoint = random.nextInt(courses.length);
+//        if (Schedule.getDEBUG()) {
+//            System.out.println("Before cross: " + this.toString());
+//            System.out.println("Crossing w/:  " + toCrossWith.toString());
+//            System.out.println("CrossoverPoint=" + crossoverPoint);
+//        }
+
+        for (int i = 0; i <= crossoverPoint; ++i) {
+            courses[i] = toCrossWith.courses[i].clone();
+        }
+        storedFitness = calculateFitness();
+//        if (Schedule.getDEBUG()) {
+//            System.out.println("After cross:  " + this.toString());
+//        }
+    }
+
+    public void mutate() {
+        // randomise one of the scheduled courses
+//        if (Schedule.getDEBUG()) {
+//            System.out.println("Before mutate: " + this.toString());
+//        }
+
+        final int mutateGene = random.nextInt(courses.length);
+        courses[mutateGene] = new ScheduledCourse(courses[mutateGene].getCourse());
+        storedFitness = calculateFitness();
+        if (Schedule.getDEBUG()) {
+            System.out.println("@" + mutateGene + " After mutate:  " + this.toString());
+        }
+    }
+
+    private int calculateFitness() {
+        // Weight of any "required" fitness is 100, such as two courses overlapping
+        // Weight of any "preferable" fitness is 1, such as keeping a 2-hour lecture as one block vs. two blocks or lecture not being at 8am
+        // TODO: Tweak these fitness weights
+
+        isValidSolution = true;
+
+        int fitnessFromOverlappingClasses = DefaultGroovyMethods.size(Course.getAllCourses()) * 100;
+        // TODO: O(n^2) iterative search. Needs improvement. Ideas: Hash array, make sure no collisions. "Sort" array and then compare linearly.
+        for (int i = 0; i < courses.length; ++i) {
+            for (int j = i + 1; j < courses.length; ++j) {
+                if (i != j) {
+                    if (courses[i].equals(courses[j])) {
+                        fitnessFromOverlappingClasses -= 100;
+                        isValidSolution = false;
+                    }
+
+                }
+
+            }
+
+        }
+        return fitnessFromOverlappingClasses;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(storedFitness).append(": ");
+        for (ScheduledCourse course : courses) {
+            s.append(course.toString()).append(", ");
+        }
+
+        return s.toString();
+    }
+
+    public int compareTo(Chromosome o) {
+        return o.getStoredFitness() - storedFitness;
+    }
+
+    public int getStoredFitness() {
+        return storedFitness;
+    }
+
+    public boolean isValidSolution() {
+        return isValidSolution;
+    }
+}
