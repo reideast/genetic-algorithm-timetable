925659f5aeff55dff892f966994dec385e43d6aa
Andrew Reid East <andrew@andreweast.net>
2019-03-18 10:36:08 +0000

    Reset all for Spring

--- src/main/java/net/andreweast/bean/Hello.java
+++
@@ -1,7 +0,0 @@
-package net.andreweast.bean;
-
-public class Hello {
-    public String getHello()  {
-        return "Hello World, I'm gradle running on AWS, and this is from a Java Class!";
-    }
-}
