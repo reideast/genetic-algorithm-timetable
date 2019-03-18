1657a958598e8187d4423dc5557b92c385002a17
Andrew Reid East <andrew@andreweast.net>
2019-03-18 13:19:39 +0000

    Make changes to spring & eb configs to create a running API on Elastic Beanstalk

--- src/main/java/net/andreweast/hello/GreetingController.java
+++ src/main/java/net/andreweast/hello/GreetingController.java
@@ -15,6 +15,11 @@ public class GreetingController {
 
     @RequestMapping(value = "/greeting", method = GET)
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println(System.getProperty("SERVER_PORT"));
+        System.out.println(System.getProperty("RDS_HOSTNAME"));
+        System.out.println(System.getProperty("SPRING_DATASOURCE_URL"));
+        System.out.println(System.getProperty("spring.datasource.url"));
+
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
