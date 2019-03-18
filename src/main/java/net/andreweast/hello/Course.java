f12c21d8147b2e15d8db363c6f5aca97053734ff
Andrew Reid East <andrew@andreweast.net>
2019-03-18 13:43:17 +0000

    Database CRUD with JSON-based API in Spring

---
+++ src/main/java/net/andreweast/hello/Course.java
@@ -0,0 +1,84 @@
+package net.andreweast.hello;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "courses", schema = "public", catalog = "ga_dev")
+public class Course {
+    private Long courseId;
+    private String name;
+//    private Department department;
+    private Long departmentId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_generator")
+    @SequenceGenerator(name="course_generator", sequenceName = "course_id_sequence", allocationSize = 1)
+    @Column(name = "course_id", updatable = false, nullable = false)
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = -1)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "department_id", nullable = true)
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+////    @ManyToOne(fetch = FetchType.LAZY)
+//    @ManyToOne(fetch = FetchType.EAGER)
+////    @ManyToOne
+//    @JoinColumn(name = "department_id", nullable = true)
+//    public Department getDepartment() {
+//        return department;
+//    }
+//
+//    public void setDepartment(Department department) {
+//        this.department = department;
+//    }
+//
+//    // TODO: update equals and hashCode with Dept.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course that = (Course) o;
+        return courseId == that.courseId &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseId, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", name='" + name + '\'' +
+//                ", department=" + department +
+                ", department=" + departmentId +
+                '}';
+    }
+}
