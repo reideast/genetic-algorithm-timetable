69d14d352fb389183af442c8f85e7f5da0d41418
Andrew Reid East <andrew@andreweast.net>
2019-03-20 18:05:52 +0000

    Throw custom HTTP status messages from REST start-scheduler

--- src/main/java/net/andreweast/api/SchedulerRestController.java
+++ src/main/java/net/andreweast/api/SchedulerRestController.java
@@ -1,5 +1,6 @@
 package net.andreweast.api;
 
+import net.andreweast.api.exception.DataNotFoundException;
 import net.andreweast.geneticalgorithm.Population;
 import net.andreweast.geneticalgorithm.Scheduler;
 import net.andreweast.model.Schedule;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 //@RequestMapping("/scheduler") // TODO: this RestController doesn't get configured to /api/* with the application.properties config: spring.data.rest.base-path=/api
@@ -22,21 +24,38 @@ public class SchedulerRestController {
     @ResponseStatus(HttpStatus.ACCEPTED) // Processing isn't complete, but this HTTP transaction is closed. Perfect! See: https://httpstatuses.com/202
     public JobIdJson createScheduleBatchJob(UserJson creator) {
         System.out.println("Hey, let's start a scheduling job! It's for user: " + creator.getCreatorId());
+        System.out.println(creator); // DEBUG: object is created, but creator.creatorId is null, even when body of request is:
+        /* DEBUG: This was copied/pasted from output of a REST request returning a UserJson obj
+            {
+                "creatorId": 3
+            }
+         */
 
         StringBuilder out = new StringBuilder();
         Iterable<Schedule> all = scheduleRepository.findAll();
         for (Schedule s : all) {
-            out.append(s).append("****");
+            out.append(s).append("************************************");
         }
 
-        Schedule schedule = scheduleRepository.findById(2L).orElseThrow(RuntimeException::new); // TODO: Right HTTP status
+        Schedule schedule = scheduleRepository.findById(3L).orElseThrow(DataNotFoundException::new);
         if (schedule.getGeneticAlgorithmRunning()) {
-            // TODO: throw the right kind of HTTP status
-            throw new RuntimeException("This scheduler job is already running");
-            // DEBUG: THIS DOES NOT ACTUALLY BREAK THE REST, i.e. still returns HTTP 202 ACCEPTED
+            // See: https://www.baeldung.com/exception-handling-for-rest-with-spring
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Job has already been started");
         }
         schedule.setGeneticAlgorithmRunning(true);
         scheduleRepository.save(schedule);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(10 * 1000);
+                schedule.setGeneticAlgorithmRunning(false);
+                scheduleRepository.save(schedule);
+                System.out.println("Job's done, m'lord!");
+            } catch (InterruptedException e) {
+                // There's not way to deal this now...the REST call has already returned!
+                e.printStackTrace();
+            }
+        }).start();
 
         Long jobId = schedule.getScheduleId();
         return new JobIdJson(jobId, out.toString());
@@ -75,7 +94,7 @@ class UserJson {
     private final Long creatorId;
 
     public UserJson() {
-        // DEBUG: This should be an error. Happens when calling the rest endpoint w/o any BODY
+        // DEBUG: This should not be able to be created without a creatorId, it should create and HTTP rather than silently creating a UserJson object. Happens when calling the rest endpoint w/o any BODY
         this.creatorId = null;
     }
 
@@ -85,5 +104,12 @@ class UserJson {
 
     public Long getCreatorId() {
         return creatorId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserJson{" +
+                "creatorId=" + creatorId +
+                '}';
     }
 }
