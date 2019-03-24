f42c5bbb20c94384b9222c96bf951b2becfe43f4
Andrew Reid East <andrew@andreweast.net>
2019-03-24 01:54:35 +0000

    Add Swagger UI to /swagger-ui.html to document all REST API endpoints automatically; enables interactive exploration

---
+++ src/main/java/net/andreweast/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package net.andreweast;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * Loads auto-generated API documentation at /swagger-ui.html
+ * See: https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+ * See: https://reflectoring.io/documenting-spring-data-rest-api-with-springfox/
+ */
+@Configuration
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
