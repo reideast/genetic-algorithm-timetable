bbf7aa717669cb351cde7012c1865338338a5384
Andrew Reid East <andrew@andreweast.net>
2019-03-24 23:03:28 +0000

    For React tutorial, add better author credit and license

--- src/main/js/client.js
+++ src/main/js/client.js
@@ -1,5 +1,22 @@
 'use strict';
-
+/**
+ * @author Greg Turnquist
+ */
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // Client.js is written by: https://spring.io/guides/tutorials/react-and-spring-data-rest/
 // It is needed for the HATEOAS REST Client. It adds support for HAL, URI Templates, and more
 // The object it exports is a function used to make AJAX calls to the REST API
