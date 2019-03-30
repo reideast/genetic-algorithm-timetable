b1cac82007d6c9cac3ab677d1e8e58615a71071a
Andrew Reid East <andrew@andreweast.net>
2019-03-30 20:12:25 +0000

    Create some of the data into their own files as well as DB

---
+++ db_script/departments.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.departments (department_id, "name") VALUES(1, 'Information Technology');
+INSERT INTO public.departments (department_id, "name") VALUES(2, 'Electronic & Computer Engineering');
