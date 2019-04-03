1141c370e353a774276aac2a648d1effd85bf299
Andrew Reid East <andrew@andreweast.net>
2019-04-03 22:32:31 +0100

    Remove the verbose DB scripts used to plan the database insert method

--- db_script/departments.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.departments (department_id, "name") VALUES(1, 'Information Technology');
-INSERT INTO public.departments (department_id, "name") VALUES(2, 'Electronic & Computer Engineering');
